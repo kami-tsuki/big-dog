a4170779ce32b00fe523a7264a9758a8b90c08b4
Philipp Neuberg <philipp.neuberg@navaya.de>
2024-02-01 12:27:17 +0100

    - coded

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import shutil
 
 
 def create_folder(parent_directory, new_folder_name):
-    new_folder_path = os.path.join(parent_directory, new_folder_name)
-    if not os.path.exists(new_folder_path):
-        os.makedirs(new_folder_path)
+    new_path = os.path.join(parent_directory, new_folder_name)
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
         print(f"Folder '{new_folder_name}' created successfully in '{parent_directory}'.")
-        return new_folder_path
+        return new_path
     else:
         print(f"Folder '{new_folder_name}' already exists in '{parent_directory}'.")
         return None
@@ -24,9 +24,9 @@ def copy_template(template, destination_directory):
 
 
 if __name__ == "__main__":
-    creation_directory = "C:/"      # TODO change this to the directory you want to use
-    directory = "C:/"               # TODO change this to the directory you want to use
-    template_directory = "C:/"      # TODO change this to the directory you want to use
+    creation_directory = "C:/"  # TODO change this to the directory you want to use
+    directory = "C:/"  # TODO change this to the directory you want to use
+    template_directory = "C:/"  # TODO change this to the directory you want to use
     folder_names = get_folder_names(directory)
     for folder_name in folder_names:
         new_folder_path = create_folder(creation_directory, folder_name)
