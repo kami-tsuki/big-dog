06cfcb089e6a3c1e1e4d54909b868afe4806018b
Philipp Neuberg <philipp.neuberg@navaya.de>
2024-02-01 12:24:19 +0100

    - coded

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import shutil
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def create_folder(parent_directory, new_folder_name):
+    new_folder_path = os.path.join(parent_directory, new_folder_name)
+    if not os.path.exists(new_folder_path):
+        os.makedirs(new_folder_path)
+        print(f"Folder '{new_folder_name}' created successfully in '{parent_directory}'.")
+        return new_folder_path
+    else:
+        print(f"Folder '{new_folder_name}' already exists in '{parent_directory}'.")
+        return None
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_folder_names(parent_directory):
+    all_items = os.listdir(parent_directory)
+    names = [item for item in all_items if os.path.isdir(os.path.join(parent_directory, item))]
+    return names
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def copy_template(template, destination_directory):
+    shutil.copytree(template, destination_directory)
+
+
+if __name__ == "__main__":
+    creation_directory = "C:/"      # TODO change this to the directory you want to use
+    directory = "C:/"               # TODO change this to the directory you want to use
+    template_directory = "C:/"      # TODO change this to the directory you want to use
+    folder_names = get_folder_names(directory)
+    for folder_name in folder_names:
+        new_folder_path = create_folder(creation_directory, folder_name)
+        if new_folder_path:
+            copy_template(template_directory, new_folder_path)
